c791e90e6f9979ddd8ad4c0c53949842f7035ef8
kmaasrud <kmaasrud@outlook.com>
2020-09-04 11:14:57 +0200

    fit_beta and predict referenced a global x, not the class' property

--- src/ols.py
+++ src/ols.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import numpy as np 
+import numpy as np
 
 np.random.seed(2020)        # set a seed to ensure reproducability
+
 
 class OLS():
     """
     Class for preforming Ordinary Least Square fits without using any statistical package libraries.
     """
 
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-    
+
     def fit_beta(self):
- 
+
         # matrix inversion to find beta
-        self.beta = np.linalg.inv(x.T @ x) @ x.T @ self.y
-        
+        self.beta = np.linalg.inv(self.x.T @ self.x) @ self.x.T @ self.y
+
         return self.beta
-    
+
     def predict(self):
-        
-        p = len(self.beta) # number of features p/ complexity of model
-        self.y_hat = x @ self.beta # predicted values
-        
+
+        p = len(self.beta)  # number of features p/ complexity of model
+        self.y_hat = self.x @ self.beta  # predicted values
+
         return p, self.y_hat
-      
-# simple test for linear case      
+
+
+# simple test for linear case
 if __name__ == '__main__':
     x = np.arange(50)[:, np.newaxis]
     y = np.array([i + np.random.rand() for i in range(50)])[:, np.newaxis]
-    ols_test = OLS(x,y)
-    beta=ols_test.fit_beta()
-    p,y_hat=ols_test.predict()
+    ols_test = OLS(x, y)
+    beta = ols_test.fit_beta()
+    p, y_hat = ols_test.predict()
